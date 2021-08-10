f156bdb61c51beaebc44045533edb88404e13b79
ersoonhao <soonhao.er@gmail.com>
2021-08-10 19:58:59 +0800

    first

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,10 +4,26 @@
 // idle: maximum time, in milliseconds, that a connection can be idle before being released
 // acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error
 
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "fyp",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000, 
+//       idle: 10000
+//     }
+//   };
+
+
+
+  module.exports = {
+    HOST: "oio.cyxsqv5x99m7.us-east-2.rds.amazonaws.com",
+    USER: "admin",
+    PASSWORD: "regopq==22858",
     DB: "fyp",
     dialect: "mysql",
     pool: {
@@ -17,5 +33,4 @@ module.exports = {
       idle: 10000
     }
   };
-
 
